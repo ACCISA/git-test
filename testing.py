8eec0532d51b665fbde89e86c900cf3558ccc6ce
AC <39713625+R0MULLUS@users.noreply.github.com>
2022-10-05 15:58:23 -0400

    Cool update

--- testing.py
+++ testing.py
@@ -1,1 +1,26 @@
-pp
+import time
+
+start_time = time.time()
+
+nums = [0,1,0,3,0,12]
+print(nums)
+# position = []
+# count = 0 
+# for i in range(len(nums)):
+#     if nums[i] == 0: 
+#         count = count + 1
+#         position.append(i)
+#     else: continue
+
+# for i in range(count):
+#     nums.remove(0)
+#     nums.append(0)
+
+
+for i in range(len(nums)):
+    nums.remove(0)
+    nums.append(0)
+print(time.time() - start_time)
+
+print(nums)
+    
